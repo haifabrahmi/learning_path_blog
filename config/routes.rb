bd53cb11785f3c5e53ae08470c2708c763c07746
haifabrahmi <haifa.brahmi@esprit.tn>
2025-01-29 17:00:07 +0100

    Correction des erreurs de style avec RuboCop

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
